752910dbd8a9760106f217de9639544ade7e5493
jakariawb <166798315+jakariawb@users.noreply.github.com>
2025-02-20 11:44:12 +0600

    first commit

---
+++ oneCle.js
@@ -0,0 +1,12 @@
+function Mybutton(){
+    alert("Im button 1");
+}
+function Mybutton1(){
+    alert("Im button 2")
+}
+
+function mypara(){
+    alert("Paragraph on My Mother: A person who happily gives her whole life for the development of her children and husband is called a mother. A mother is the one who not only gives birth to her children but also takes care of her children without rest. She sacrifices not only her day but also her nights. In simple words, a mother is a name of sacrifice."
+
+    )
+}
